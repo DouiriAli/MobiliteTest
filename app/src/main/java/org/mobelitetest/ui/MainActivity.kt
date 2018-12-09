377ea1c6bf85a4f329bbe613598b3c05558b07eb
Ali Douiri <my.alidouiri@gmail.com>
2018-12-09 00:42:19 +0000

    Initial commit : Project creation

---
+++ app/src/main/java/org/mobelitetest/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package org.mobelitetest.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import org.mobelitetest.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
